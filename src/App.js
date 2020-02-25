7f0c3b16907cc15f08b5dac27e718e9b3f131a29
Otto Korhonen <otto@xd.fi>
2020-02-25 08:55:50 +0200

    Tehtävä separate components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todotable from './components/todolist';
 
 function App() {
+  const [description, setDescription] = React.useState({ Description: '', Date: '' });
+  const [todo, setTodo] = React.useState([]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodo([...todo, description]);
+  }
+
+  const inputChanged = (event) => {
+    setDescription({ ...description, [event.target.name]: event.target.value })
+  }
+
+  const deleteItem = (event) =>{
+    setTodo(todo.filter((todo, i) => i !== parseInt(event.target.id)))
+    console.log(event.target.id)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To do list</h1>
+      <form onSubmit={addTodo} id="input-container">
+        <fieldset>
+          <legend>Add to do</legend>
+        Description: <input type="text" name="description" onChange={inputChanged} value={todo.description}></input>
+        Date: <input type="date" name="date" onChange={inputChanged} value={todo.date}></input>
+        <input type="submit" value="Add"></input>
+        </fieldset>
+      </form>
+      <Todotable todo={todo} description={description} deleteItem={deleteItem} />
     </div>
   );
 }
